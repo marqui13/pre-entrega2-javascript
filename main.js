7f9f7c7bf019c896fed393318cbe3787e4fdd753
marqui13 <marquinez.13@gmail.com>
2023-04-28 21:52:49 -0500

    s

--- main.js
+++ main.js
@@ -11,17 +11,15 @@ document.getElementById('calcular').addEventListener('click', function() {
     const cuotaMensual = capital * (interesMensual * Math.pow(1 + interesMensual, cuotas)) / (Math.pow(1 + interesMensual, cuotas) - 1);
     const totalPago = cuotaMensual * cuotas;
 
-    let informacion = `
-        <p>Capital: $${capital.toFixed(2)}</p>
-        <p>Intereses anuales: ${(intereses * 100).toFixed(2)}%</p>
-        <p>Número de cuotas: ${cuotas}</p>
-        <p>Cuota mensual: $${cuotaMensual.toFixed(2)}</p>
-        <p>Total a pagar: $${totalPago.toFixed(2)}</p>
-    `;
+    document.getElementById("resultadoCapital").innerHTML = `<p>Capital: $${capital.toFixed(2)}</p>`;
+    document.getElementById("resultadoInteres").innerHTML = `<p>Intereses anuales: ${(intereses * 100).toFixed(2)}%</p>`  //las comillas invertidas se utilizan para concadenar sin utilizar el + pero utilizando las sintaxis ${}
+    document.getElementById("numeroCuotas").innerHTML = `<p>Número de cuotas: ${cuotas}</p>`
+    document.getElementById("resultadoCuota").innerHTML = `<p>Cuota mensual: $${cuotaMensual.toFixed(2)}</p>`
+    document.getElementById("resultadoTotal").innerHTML = `<p>Total a pagar: $${totalPago.toFixed(2)}</p>`
+
+
     const btnContactarAsesor = document.getElementById("contactarAsesor");
     btnContactarAsesor.classList.remove("hidden")
-
-    document.getElementById('informacion').innerHTML += informacion;
 
     // Mover la vinculación del evento 'click' aquí
     btnContactarAsesor.addEventListener('click', function() {
